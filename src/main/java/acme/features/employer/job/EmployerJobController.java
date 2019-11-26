3598e6bf305c1dc28b508714fd6cde26affdd932
sersanleo <sersanleo@alum.us.es>
2019-11-26 20:38:57 +0100

    Faltan los servicios

---
+++ src/main/java/acme/features/employer/job/EmployerJobController.java
@@ -0,0 +1,43 @@
+/*
+ * AnonymousShoutController.java
+ *
+ * Copyright (c) 2019 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.employer.job;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import acme.components.CustomCommand;
+import acme.entities.company.Company;
+import acme.framework.components.BasicCommand;
+import acme.framework.controllers.AbstractController;
+import acme.framework.entities.Authenticated;
+
+@Controller
+@RequestMapping("/authenticated/company/")
+public class EmployerJobController extends AbstractController<Authenticated, Company> {
+
+	@Autowired
+	private EmployerJobShowService		showService;
+	@Autowired
+	private EmployerJobListMineService	listMineService;
+
+
+	@PostConstruct
+	private void initialise() {
+		super.addBasicCommand(BasicCommand.SHOW, this.showService);
+		super.addCustomCommand(CustomCommand.LIST_MINE, BasicCommand.LIST, this.listMineService);
+	}
+
+}
